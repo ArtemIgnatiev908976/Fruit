cb70e179495f285190cc0cd902f9714d0ef515eb
Artem <ignatev.artem.nik@gmail.com>
2021-03-23 23:23:55 +0400

    Обьявляем переменные для классов

---
+++ js/script.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+
+
+const tabs =document.querySelectorAll('.tabheader__item');
+const tabsContent = document.querySelectorAll('.tabcontent');
+const tabsParent = document.querySelector('.tabheader__items');
+
+
+
+
+
+
+
+
+});
