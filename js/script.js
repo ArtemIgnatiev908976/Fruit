2c7340c1ba1c3e2c82611e3d3db76ffb4deac94c
Artem <ignatev.artem.nik@gmail.com>
2021-04-01 12:59:34 +0400

    красивое оповещение формы и спинер

--- js/script.js
+++ js/script.js
@@ -107,7 +107,7 @@ document.addEventListener('DOMContentLoaded', () => {
     //Модальное окно
     const modalTrigger = document.querySelectorAll('[data-modal]');
     const modal = document.querySelector('.modal');
-    const modalCloseBtn = document.querySelector('[data-close]');
+    // const modalCloseBtn = document.querySelector('[data-close]');
     //добавили на все кнопки перебором forEach
     modalTrigger.forEach(btn => {
         btn.addEventListener('click', openModal)
@@ -134,13 +134,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // функция закрытия модалки
 
-    modalCloseBtn.addEventListener('click', closeModal)
-        //Модальное окно
+    // modalCloseBtn.addEventListener('click', closeModal)
+    //Модальное окно
 
 
     //Закрывать модалку если кликать вне заны моодалки
     modal.addEventListener('click', (e) => {
-            if (e.target === modal) {
+            if (e.target === modal || e.target.getAttribute('data-close') == '') {
                 closeModal();
             }
         })
@@ -281,7 +281,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const forms = document.querySelectorAll('form');
 
     const message = {
-        loading: 'Закгузка',
+        loading: 'img/form/spinner.svg',
         success: 'Спасибо! Скоро мы с вами свяжемся',
         failure: 'Что то пошло не так...'
     };
@@ -296,10 +296,14 @@ document.addEventListener('DOMContentLoaded', () => {
         form.addEventListener('submit', (e) => {
             e.preventDefault();
 
-            const statusMessage = document.createElement('div');
-            statusMessage.classList.add('status');
-            statusMessage.textContent = message.loading;
-            form.append(statusMessage);
+            const statusMessage = document.createElement('img');
+            statusMessage.src = message.loading;
+            statusMessage.style.cssText = `
+            display:block;
+            margin: 0 auto;
+            `;
+
+            form.insertAdjacentElement('afterend', statusMessage);
 
 
 
@@ -324,20 +328,50 @@ document.addEventListener('DOMContentLoaded', () => {
             request.addEventListener('load', () => {
                 if (request.status === 200) {
                     console.log(request.response);
-                    statusMessage.textContent = message.success;
+                    showThanksModal(message.success);
                     form.reset();
-                    setTimeout(() => {
-                        statusMessage.remove();
-                    }, 2000);
+                    statusMessage.remove();
+
                 } else {
-                    statusMessage.textContent = message.failure;
+                    showThanksModal(message.failure);
                 }
             })
 
         })
     }
 
+    //Реализация скрипта для отправки данных
+    //Forms
 
 
+
+    //красивое оповещение пользователя
+
+    function showThanksModal(message) {
+        const prevModalDialog = document.querySelector('.modal__dialog');
+
+        prevModalDialog.classList.add('hide');
+        openModal();
+
+        const thanksModal = document.createElement('div');
+        thanksModal.classList.add('.modal__dialog');
+        thanksModal.innerHTML = `
+        <div class="modal__content">
+        <div class="modal__close" data-close>×</div> 
+        <div class="modal__title">${message}</div>  
+        </div>
+        
+        `;
+
+
+        document.querySelector('.modal').append(thanksModal);
+        setTimeout(() => {
+            thanksModal.remove();
+            prevModalDialog.classList.add('show');
+            prevModalDialog.classList.remove('hide');
+            closeModal();
+        }, 4000);
+    }
+    //красивое оповещение пользователя
 
 });
