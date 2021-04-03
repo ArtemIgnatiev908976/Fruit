0b109146c634141bd443863bcffaab40fb75b3d1
Artem <ignatev.artem.nik@gmail.com>
2021-04-03 14:50:53 +0400

    Библиотека axios

--- js/script.js
+++ js/script.js
@@ -244,10 +244,18 @@ document.addEventListener('DOMContentLoaded', () => {
         //            new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
         //         });
         //     })
-        // ////используем классы для карточек
 
-    getResource(' http://localhost:3000/menu')
-        .then(data => createCard(data));
+    axios.get(' http://localhost:3000/menu')
+        .then(data => {
+            data.data.forEach(({ img, altimg, title, descr, price }) => {
+                new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+            });
+        });
+
+    // ////используем классы для карточек
+
+    // getResource(' http://localhost:3000/menu')
+    //     .then(data => createCard(data));
 
     function createCard(data) {
         data.forEach(({ img, altimg, title, descr, price }) => {
